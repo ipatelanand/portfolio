dff38d11fd791d9d7428518b66443f94579c005f
ipatelanand@gmail.com <ipatelanand@gmail.com>
2021-03-11 17:00:12 -0500

    added projects

---
+++ app.js
@@ -0,0 +1,12 @@
+// import from 'http-client'
+
+// var client = new HttpClient()
+// client.get("https://resinvest.herokuapp.com/", function (response) {
+// 	// do something with response
+// })
+
+// client.get("https://tweet-city.herokuapp.com/home", function (response) {
+// 	// do something with response
+// })
+
+// console.log("Wecome!")
